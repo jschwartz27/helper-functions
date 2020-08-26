a69b336b00124c4ef93a8efe8177d827676931e2
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2020-08-26 13:46:29 +0200

    yaml/json loading functions

---
+++ load_save_data.py
@@ -0,0 +1,27 @@
+
+### YAML FUNCTIONS ###
+import yaml
+
+
+def load_yml(filename: str):
+    with open(filename + ".yaml") as stream:
+        return yaml.load(stream, Loader=yaml.FullLoader)
+
+
+def save_yml(data, filename: str) -> None:
+    with open(r'{}.yaml'.format(filename), 'w') as file:
+        yaml.dump(data, file)
+
+
+### JSON FUNCTIONS ###
+import json
+
+
+def load_json(filename: str):
+    with open(filename + ".json") as file:
+        return json.load(file)
+
+
+def save_json(data, filename: str) -> None:
+    with open(filename + ".json", 'w') as file:
+        json.dump(data, file, indent=4)  # sort_keys=True ensure_ascii=False
